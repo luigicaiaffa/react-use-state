90d8ffb8ff56d2fdeceaf4b7b9cfb5af636a01e5
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-12-02 15:42:05 +0100

    add card

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,1 +1,9 @@
-export default function Header() {}
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1 className="title-header">Learn Web Development</h1>
+      </div>
+    </header>
+  );
+}
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,1 +1,36 @@
-export default function Main() {}
+import { languages } from "../../data/languages";
+
+export default function Main() {
+  return (
+    <main>
+      <div className="container">
+        <section>
+          <nav className="my-4 navbar navbar-expand">
+            <ul className="navbar-nav">
+              {languages.map((language) => {
+                return (
+                  <li key={language.id} className="nav-item list-btn">
+                    <button className="btn btn-primary">
+                      {language.title}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </section>
+
+        <section>
+          {
+            <div key={languages[0].id} className="card">
+              <div className="card-body">
+                <h5 className="card-title">{languages[0].title}</h5>
+                <p className="card-text">{languages[0].description}</p>
+              </div>
+            </div>
+          }
+        </section>
+      </div>
+    </main>
+  );
+}
